cc55e8df56c90cee565fca14f88b3dbad8a06ee5
Rina <Rina@103.10.61.1>
2017-12-11 14:17:44 +0700

    f

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -1,5 +1,12 @@
 package hello;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
@@ -31,4 +38,24 @@ public class GreetingController {
     	return data.stream().filter(line->line.startsWith(prefix)).collect(Collectors.toList());
     }
     
+    @RequestMapping("/countries")
+    public String getCountries() throws IOException{
+    URL url = new URL ("http://www.webservicex.net/globalweather.asmx/GetCitiesByCountry");
+    URLConnection connection = url.openConnection();
+    connection.setDoOutput(true);
+    connection.setRequestProperty("Content-Type","application/x-www-form-urlencoded");
+    connection.setRequestProperty("Content-Length", "0");
+    InputStream stream = url.openConnection().getInputStream();
+    InputStreamReader reader = new InputStreamReader(stream);
+    BufferedReader buffer = new BufferedReader(reader);
+    
+    String line;
+    StringBuilder builder = new StringBuilder();
+    while((line = buffer.readLine()) != null) {
+    	builder.append(line);
+    }
+    
+  return builder.toString();
+    
+    }    
 }
